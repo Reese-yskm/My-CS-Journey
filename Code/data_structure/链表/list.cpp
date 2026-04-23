856f3734b5c9f1f1b8a0607ac08af5456935b9a3
Reese-yskm <wxr0689@163.com>
2026-04-23 16:41:57 +0800

    Add: 新增链表删除、反转和交换节点的功能实现

--- Code/data_structure/链表/list.cpp
+++ Code/data_structure/链表/list.cpp
@@ -43,6 +43,44 @@ void print__each_value(Listnode *head)
     cout << "\n";
 }
 
+Listnode *deleteElement(Listnode *head, int x)
+{
+    Listnode *dummy = new Listnode(0);
+    dummy->next = head;
+    Listnode *cur = dummy;
+    while (cur && cur->next)
+    {
+        if (cur->next->val == x)
+        {
+            cur->next = cur->next->next;
+        }
+        else
+        {
+            cur = cur->next;
+        }
+    }
+    return dummy->next;
+}
+
+Listnode *deleteKthElementEnd(Listnode *head, int k)
+{
+    Listnode *dummy = new Listnode(0);
+    dummy->next = head;
+    Listnode *slow = dummy;
+    Listnode *quick = dummy;
+    for (int i = 0; i < k; i++)
+    {
+        quick = quick->next;
+    }
+    while (quick->next)
+    {
+        slow = slow->next;
+        quick = quick->next;
+    }
+    slow->next = slow->next->next;
+    return dummy->next;
+}
+
 Listnode *reverseList(Listnode *head)
 {
     Listnode *pre = nullptr;
@@ -88,7 +126,7 @@ Listnode *findKthElementEnd(Listnode *head, int k)
 {
     Listnode *slow = head;
     Listnode *fast = head;
-    for (int i = 0; i < k - 1; i++)
+    for (int i = 0; i < k; i++)
     {
         if (fast == nullptr)
         {
@@ -104,42 +142,110 @@ Listnode *findKthElementEnd(Listnode *head, int k)
     }
     return slow;
 }
+
+Listnode *swapPairs(Listnode *head)
+{
+    Listnode *dummy = new Listnode(0);
+    dummy->next = head;
+
+    Listnode *pre = dummy;
+
+    while (pre->next && pre->next->next)
+    {
+        Listnode *a = pre->next;
+        Listnode *b = a->next;
+        Listnode *n = b->next;
+        pre->next = b;
+        a->next = n;
+        b->next = a;
+        pre = a;
+    }
+
+    return dummy->next;
+}
+
+Listnode *reverseKGroup(Listnode *head, int k)
+{
+    Listnode *dummy = new Listnode(0);
+    dummy->next = head;
+    Listnode *pre = dummy;
+
+    while (true)
+    {
+        Listnode *end = pre;
+
+        // 1. 找到这一组最后一个节点
+        for (int i = 0; i < k; i++)
+        {
+            end = end->next;
+            if (end == nullptr)
+                return dummy->next;
+        }
+
+        // 2. 记录关键节点
+        Listnode *start = pre->next;
+        Listnode *nextGroup = end->next;
+
+        // 3. 切断
+        end->next = nullptr;
+
+        // 4. 反转并接回去
+        pre->next = reverseList(start);
+        start->next = nextGroup;
+
+        // 5. 更新 pre
+        pre = start;
+    }
+}
 int main()
 {
-    vector<int> arr = {4, 7, 2, 5, 1};
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7};
     Listnode *my_list = init_List(arr);
 
     cout << "Original list: ";
     print__each_value(my_list);
 
-    // Check for cycle
-    cout << (hasCycle(my_list) ? "Cycle detected" : "No cycle") << "\n";
+    // // Check for cycle
+    // cout << (hasCycle(my_list) ? "Cycle detected" : "No cycle") << "\n";
 
-    // Find and print middle node
-    Listnode *middle = findMiddle(my_list);
-    if (middle)
-    {
-        cout << "Middle node: " << middle->val << "\n";
-    }
-    else
-    {
-        cout << "List is empty\n";
-    }
+    // // Find and print middle node
+    // Listnode *middle = findMiddle(my_list);
+    // if (middle)
+    // {
+    //     cout << "Middle node: " << middle->val << "\n";
+    // }
+    // else
+    // {
+    //     cout << "List is empty\n";
+    // }
 
-    // Find and print the 3rd node
-    int k = 2;
-    Listnode *kth = findKthElementEnd(my_list, k);
-    if (kth)
-    {
-        cout << "The " << k << "th node from end is: " << kth->val << "\n";
-    }
-    else
-    {
-        cout << "The list is too short to find the " << k << "the node from end.\n";
-    }
+    // // Find and print the 3rd node
+    // int k = 2;
+    // Listnode *kth = findKthElementEnd(my_list, k);
+    // if (kth)
+    // {
+    //     cout << "The " << k << "th node from end is: " << kth->val << "\n";
+    // }
+    // else
+    // {
+    //     cout << "The list is too short to find the " << k << "the node from end.\n";
+    // }
 
-    Listnode *reversed_list = reverseList(my_list);
-    cout << "Reversed list: ";
-    print__each_value(reversed_list);
+    // int x = 7;
+    // cout << "Delete the val " << x << " :";
+    // print__each_value(deleteElement(my_list, x));
+
+    // cout << "Delete the 3th node from end: ";
+    // print__each_value(deleteKthElementEnd(my_list, 2));
+
+    // Listnode *reversed_list = reverseList(my_list);
+    // cout << "Reversed list: ";
+    // print__each_value(reversed_list);
+
+    // cout << "swap in pair: ";
+    // print__each_value(swapPairs(my_list));
+
+    cout << "swap in k group: ";
+    print__each_value(reverseKGroup(my_list, 3));
     return 0;
 }
